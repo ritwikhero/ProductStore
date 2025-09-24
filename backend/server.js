7c2e72250d86150ba9cee2131e93102707fcd298
Ritwik <ritwikhero@gmail.com>
2025-09-25 01:30:28 +0530

    APIs created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
@@ -28,6 +29,52 @@ app.post("/api/products", async (req, res) => {
     return res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
     console.log("Error in create product", error.message);
+    return res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    return res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.log("Error in delete product", error.message);
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    return res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in get products", error.message);
+    return res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid product id" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    return res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
     return res.status(500).json({ success: false, message: "Server Error" });
   }
 });
